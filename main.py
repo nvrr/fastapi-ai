ef106899351f27ef80ca3a912efc782b38297c3e
nvrr <sureshvarma809@gmail.com>
2026-09-04 10:19:04 +0530

    begining

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+
+def create_application():
+    application = FastAPI()
+    return application
+
+
+app = create_application()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello nvr!"}
+
