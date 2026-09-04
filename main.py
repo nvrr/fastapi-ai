d10656dc3956ba05fb515345c74d565731994fa6
nvrr <sureshvarma809@gmail.com>
2026-09-04 16:21:50 +0530

    modified main.py xampp mysql setup added two lines of code, engine, and imported users to recognise and

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-
+from app.config.database import Base, engine
+from app.models.user import User
 
 def create_application():
+     # Create database tables
+    print("Tables:", Base.metadata.tables.keys())
+    Base.metadata.create_all(bind=engine)
+
     application = FastAPI()
     return application
 
